f49b5d2b1b99140841901d92388acb46705316c9
Michael McLeod <michaelmcleod@ucl.ac.uk>
2025-01-08 21:08:35 +0000

    Add naive omp pragma

--- pi/main.cpp
+++ pi/main.cpp
@@ -15,6 +15,7 @@ int main() {
   auto start = high_resolution_clock::now();
 
   double sum = 0.0f;
+  #pragma omp parallel for
   for(int i=0; i<N; ++i) {
     double x = i*dx;
     sum += 4.0f/(1.0f + x*x)*dx;
